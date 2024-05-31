30cb780371a6b39c406dcf8a6e2762cced2f137c
Sam Ruby <rubys@intertwingly.net>
2024-05-30 22:52:05 -0400

    after fly launch

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
       <h1 className="text-2xl">Welcome to your Next.js app!</h1>
       <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        className="relative"
         src="/next.svg"
         alt="Next.js Logo"
         width={180}
         height={37}
+        priority
+      />
+      <Image
+        className="relative"
+        src="/flydotio.svg"
+        alt="Fly.io Logo"
+        width={180}
+        height={163}
         priority
       />
     </main>
